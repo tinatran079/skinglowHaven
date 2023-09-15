7efb1593a3875693b35e316df5ac9b67e9040ec0
Tina Tran <tinatran079@gmail.com>
2023-09-15 17:28:41 -0500

    fetchproducts

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
+import { React, useState, useEffect } from 'react';
+import {useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import axios from 'axios';
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
+
     const { id: productId } = useParams();
-    //fetch product based on id
-    const product = products.find((p) => p._id === productId)
-    console.log(product)
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
 
   return (
     <>
