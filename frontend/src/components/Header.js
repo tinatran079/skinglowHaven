a5ac96ba8dd7d888aa634ffa82c0e31518db0771
Tina Tran <tinatran079@gmail.com>
2023-09-28 17:25:55 -0500

    cart screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge,Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap"
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -22,6 +25,13 @@ const Header = () => {
             <LinkContainer to="/cart">
               <Nav.Link>
                 <FaShoppingCart /> Cart
+                {
+                  cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                      {cartItems.reduce((acc, cur) => acc + cur.qty, 0)}
+                    </Badge>
+                  )
+                }
               </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
