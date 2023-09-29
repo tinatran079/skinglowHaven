ab589875ef2e291cee128fdab73f7aa05152e39d
Tina Tran <tinatran079@gmail.com>
2023-09-29 15:29:32 -0500

    user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,13 +5,26 @@ import User from "../models/userModel.js";
 // @route  POST /api/users/login
 //@access  Public
 const authUser = asyncHandler (async (req,res) => {
-    res.send('auth user');
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401); //unauthorized
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc   Register user
 // @route  POST /api/users
 //@access  Public
 const registerUser = asyncHandler (async (req,res) => {
+
     res.send('register user');
 });
 
