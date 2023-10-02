b1a17a3e002afe7c92d7719d9e4e714b91e0aa8b
Tina Tran <tinatran079@gmail.com>
2023-10-02 17:40:58 -0500

    login functional

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge,Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap"
 import { useSelector } from "react-redux";
@@ -8,6 +8,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    console.log('logout');
+  };
 
   return (
     <header>
@@ -34,11 +39,20 @@ const Header = () => {
                 }
               </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-              <Nav.Link>
+              { userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (<LinkContainer to="/login">
+              <Nav.Link href='/login'>
                 <FaUser /> Sign In
               </Nav.Link>
-              </LinkContainer>
+              </LinkContainer>) }
             </Nav>
           </Navbar.Collapse>
         </Container>
