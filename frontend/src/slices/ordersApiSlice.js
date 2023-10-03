69fa830c4ecca27556d464adf7c06056033d06a4
Tina Tran <tinatran079@gmail.com>
2023-10-03 16:05:08 -0500

    creating an order

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -7,10 +7,10 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
             query: (order) => ({
                 url: ORDERS_URL,
                 method: 'POST',
-                body: {...order}
+                body: order,
             })
         })
     })
 });
 
-export const {useCreateOrderMutaion} = ordersApiSlice;
+export const {useCreateOrderMutation} = ordersApiSlice;
