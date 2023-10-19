387d5fab3031e9869f437fa052f29e27521f394b
Tina Tran <tinatran079@gmail.com>
2023-10-19 16:46:49 -0500

    admin route components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { LinkContainer } from "react-router-bootstrap"
+import { LinkContainer } from "react-router-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
@@ -22,9 +22,9 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      navigate('/login')
+      navigate("/login");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
 
@@ -32,41 +32,54 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-        <LinkContainer to='/'>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ProShop" />
-            Proshop
-          </Navbar.Brand>
-        </LinkContainer>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">
+              <img src={logo} alt="ProShop" />
+              Proshop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <LinkContainer to="/cart">
-              <Nav.Link>
-                <FaShoppingCart /> Cart
-                {
-                  cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{marginLeft:'5px'}}>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
                       {cartItems.reduce((acc, cur) => acc + cur.qty, 0)}
                     </Badge>
-                  )
-                }
-              </Nav.Link>
+                  )}
+                </Nav.Link>
               </LinkContainer>
-              { userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
                     Logout
                   </NavDropdown.Item>
                 </NavDropdown>
-              ) : (<LinkContainer to="/login">
-              <Nav.Link href='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
-              </LinkContainer>) }
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link href="/login">
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
