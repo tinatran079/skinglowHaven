98f23d17697b42630293a9ccc06a1f9e278ca789
Tina Tran <tinatran079@gmail.com>
2023-11-08 16:22:51 -0600

    some small fixes

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -39,6 +39,7 @@ const cartSlice = createSlice({
       state.cartItems = [];
       return updateCart(state);
     },
+    resetCart: (state) => (state = initialState),
   },
 });
 
@@ -48,6 +49,7 @@ export const {
   saveShippingAddress,
   savePaymentMethod,
   clearCartItems,
+  resetCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
