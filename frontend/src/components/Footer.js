c2d23dac0a2ec6c276760be70d5e6ffbd48c7c44
Tina Tran <tinatran079@gmail.com>
2023-11-15 16:32:19 -0600

    changed favicon

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-        <Container>
-            <Row>
-                <Col className="text-center py-3">
-                    <p>ProShop &copy; {currentYear}</p>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>SkinGlow Haven &copy; {currentYear}</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
